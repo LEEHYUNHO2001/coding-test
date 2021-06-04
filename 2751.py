432d030fa57e413bd79005973dd7552ba4d9fa94
dlgugh2001 <dlgugh2001@naver.com>
2021-06-04 16:53:28 +0900

    2021.06.04

---
+++ 2751.py
@@ -0,0 +1,55 @@
+#https://www.acmicpc.net/problem/2751
+#수 정렬하기 2
+
+
+
+
+#합병정렬 - 재귀 이용
+import sys
+
+def merge_sort(array):
+    if len(array)<=1:
+        return array
+    
+    # 재귀함수를 이용해서 끝까지 분할
+    mid = len(array)//2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    i,j,k = 0,0,0
+
+    # 분할된 배열끼리 비교
+    while i<len(left) and j <len(right):
+        if left[i]<right[j]:
+            array[k] = left[i]
+            i += 1
+        else:
+            array[k] = right[j]
+            j += 1
+        k+=1
+    
+    # 먼저 끝났을 때 
+    if i==len(left):
+        while j < len(right):
+            array[k] = right[j]
+            j+=1
+            k+=1
+    elif j==len(right):
+        while i < len(left):
+            array[k] = left[i]
+            i+=1
+            k+=1
+    return array
+
+N = int(input())
+array = []
+
+for i in range(N):
+    num = int(sys.stdin.readline())
+    array.append(num)
+
+merge_array = merge_sort(array)
+
+for i in merge_array:
+    print(i)
+
