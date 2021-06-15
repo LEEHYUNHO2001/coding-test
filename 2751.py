2f3f5a606ed242389da68e6b747f387c7e03839e
dlgugh2001 <dlgugh2001@naver.com>
2021-06-15 17:19:15 +0900

    2021.06.15

--- 2751.py
+++ 2751.py
@@ -1,6 +1,43 @@
 #https://www.acmicpc.net/problem/2751
 #수 정렬하기 2
 
+#퀵 정렬 -> 시간 초과
+import sys
+
+def Q(lst, left, right):
+    if(left < right):
+        a = left
+        c = right
+        p = lst[(left + right)//2]
+        
+        while a <= c:
+            while lst[a] < p:
+                a += 1
+            
+            while lst[c] > p:
+                c -= 1
+            
+            if a <= c:
+                lst[a], lst[c] = lst[c], lst[a]
+                a += 1
+                c -= 1
+
+        Q(lst, left, c)
+        Q(lst, a, right)
+
+N = int(input())
+lst = []
+
+for i in range(N):
+    num = int(sys.stdin.readline().strip())
+    lst.append(num)
+
+Q(lst, 0, N - 1)
+
+for i in range(N):
+    print(lst[i])
+
+'''
 #합병정렬 - 재귀 이용
 import sys
 
@@ -50,3 +87,4 @@ merge_array = merge_sort(array)
 for i in merge_array:
     print(i)
 
+'''
