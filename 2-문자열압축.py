ac08c5b97cb46f69f95b8a39593e378cb0090a4d
dlgugh2001 <dlgugh2001@naver.com>
2021-06-23 17:27:14 +0900

    2021.06.23

--- 2-문자열압축.py
+++ 2-문자열압축.py
@@ -2,36 +2,45 @@ def z(s, i):
     lst = []
     j = 0
     cnt = 0
+    
+    #i만큼 잘라서 배열로 저장
     while j < len(s):
         lst.append(s[j:j+i])
         j += i
-    
     lst2 = lst.copy()
-
+    
     for k in range(len(lst) -1):
         if lst[k] == lst[k+1]:
             cnt += 1
             lst2.pop(0)
+            #마지막 문자까지 같을경우
             if k + 1 == len(lst) - 1:
-                lst2.append('A')
+                #10개면 2개, 100개면 3개 추가
+                num = len(str(cnt+1))
+                for j in range(num):
+                    lst2.append('A')
         else:
+            #같은거 있어서 pop된 상태에서 다른거 나왔을 경우
             if cnt > 0:
-                lst2.append('A')
+                #10개면 2개, 100개면 3개 추가
+                num = len(str(cnt+1))
+                for j in range(num):
+                    lst2.append('A')
+                
             cnt = 0
+            
     result = ''.join(map(str, lst2))
     
     return len(result)
 
 def solution(s):
-    answer = len(s)
-    result = 0
+    answer = []
     
     if len(s) == 1:
         return 1
     
-    for i in range(1, len(s)):
-        result = z(s ,i)
-        if result < answer:
-            answer = result
+    for i in range(1, len(s)//2 + 1):
+        answer.append(z(s ,i))
+
     
-    return answer
+    return min(answer)
