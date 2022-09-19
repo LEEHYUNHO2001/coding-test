0a65ea64c84be9d2bd7afa25e8444d176c1c567a
HyunHo Lee <dlgusgh2001@naver.com>
2022-09-19 21:39:19 +0900

    2022.09.19

--- programers/2-[1]차캐시.js
+++ programers/2-[1]차캐시.js
@@ -1,3 +1,29 @@
+// splice 이용
+function solution(cacheSize, cities) {
+  var answer = 0;
+  let cache = [];
+  if (cacheSize === 0) return cities.length * 5;
+
+  cities.forEach((data) => {
+    const city = data.toLowerCase();
+    if (!cache.includes(city) && cache.length < cacheSize) {
+      cache.push(city);
+      answer += 5;
+    } else if (!cache.includes(city) && cache.length === cacheSize) {
+      cache.shift();
+      cache.push(city);
+      answer += 5;
+    } else {
+      const idx = cache.indexOf(city);
+      cache.splice(idx, 1);
+      cache.push(city);
+      answer += 1;
+    }
+  });
+  return answer;
+}
+
+// 히트된 요소 filter로 거르기
 function solution(cacheSize, cities) {
   var answer = 0;
   let cache = [];
