0a00c9f8aee56870f5c09d2e2135ebfa409d8dff
HyunHo Lee <dlgusgh2001@naver.com>
2023-07-01 15:00:33 +0900

    2023.07.01

--- programers/1-카드뭉치.js
+++ programers/1-카드뭉치.js
@@ -11,3 +11,21 @@ function solution(cards1, cards2, goal) {
   });
   return answer ? "Yes" : "No";
 }
+
+// 재귀
+function solution(cards1, cards2, goal) {
+  if (goal.length === 0) {
+    return "Yes"; // 기저 조건: goal 배열이 모두 소진되었을 때
+  }
+
+  const word = goal[0];
+  if (word === cards1[0]) {
+    cards1.shift(); // cards1에서 해당 단어 사용
+    return solution(cards1, cards2, goal.slice(1)); // 재귀 호출
+  } else if (word === cards2[0]) {
+    cards2.shift(); // cards2에서 해당 단어 사용
+    return solution(cards1, cards2, goal.slice(1)); // 재귀 호출
+  }
+
+  return "No"; // 단어를 사용할 수 없는 경우
+}
