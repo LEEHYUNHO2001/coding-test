cd97a79bf3ac9002881665c1df8fc8d13a7015e7
HyunHo Lee <dlgusgh2001@naver.com>
2023-07-04 21:55:36 +0900

    2023.07.04

--- programers/1-문자열나누기.js
+++ programers/1-문자열나누기.js
@@ -1,21 +1,30 @@
 function solution(s) {
-  let stack = [];
-  const arr = s.split("");
-  let answer = 0;
+  // let stack = [];
+  // const arr = s.split('');
+  // let answer = 0;
+  // arr.forEach((str) => {
+  //     stack.push(str);
+  //     const sameLength = stack.filter((item) => item === stack[0]).length;
+  //     const differentLength = stack.filter((item) => item !== stack[0]).length;
+  //     if (sameLength === differentLength) {
+  //         answer += 1;
+  //         stack = [];
+  //     }
+  // });
+  // if (stack.length > 0) answer += 1;
+  // return answer;
 
-  arr.forEach((str) => {
-    stack.push(str);
-    const sameLength = stack.filter((item) => item === stack[0]).length;
-    const differentLength = stack.filter((item) => item !== stack[0]).length;
-    if (sameLength === differentLength) {
-      answer += 1;
-      stack = [];
+  const arr = s.split("");
+  const stack = [];
+  let x = 0;
+  let cnt = 0;
+  arr.forEach((num, index) => {
+    if (num === arr[x]) cnt++;
+    else cnt--;
+    if (cnt === 0) {
+      stack.push(arr.slice(x, index + 1).join(""));
+      x = index + 1;
     }
   });
-
-  if (stack.length > 0) {
-    answer += 1;
-  }
-
-  return answer;
+  return stack.join("").length === s.length ? stack.length : stack.length + 1;
 }
