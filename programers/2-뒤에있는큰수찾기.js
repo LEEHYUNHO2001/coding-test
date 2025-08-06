9df6aac0a463fc355267157e717b356023a1c672
ayaan <dlgusgh2001@naver.com>
2025-08-06 21:02:28 +0900

    2025-08-06

--- programers/2-뒤에있는큰수찾기.js
+++ programers/2-뒤에있는큰수찾기.js
@@ -1,8 +1,25 @@
 // 시간 초과
+// function solution(numbers) {
+//   return numbers.map((cur, index, arr) => {
+//     const rest = arr.slice(index + 1);
+//     const backLargeNum = rest.find((num) => num > cur);
+//     return backLargeNum !== undefined ? backLargeNum : -1;
+//   });
+// }
+
 function solution(numbers) {
-  return numbers.map((cur, index, arr) => {
-    const rest = arr.slice(index + 1);
-    const backLargeNum = rest.find((num) => num > cur);
-    return backLargeNum !== undefined ? backLargeNum : -1;
-  });
+  const stack = [];
+
+  return numbers.reduceRight((acc, current, index) => {
+    while (stack.length && stack[stack.length - 1] <= current) {
+      stack.pop();
+    }
+
+    const nextGreater = stack.length ? stack[stack.length - 1] : -1;
+    stack.push(current);
+
+    acc[index] = nextGreater;
+
+    return acc;
+  }, new Array(numbers.length));
 }
